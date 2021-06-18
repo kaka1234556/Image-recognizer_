ea2e8409333c8175cc6c596da8b4a1daf398479b
kaka1234556 <80517682+kaka1234556@users.noreply.github.com>
2021-06-18 20:02:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+
+   
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+  camera = document.getElementById("camera");
+  Webcam.attach( '#camera' ); 
+
+  function take_snapshot()
+  {
+    Webcam.snap(function(data_uri)
+    {
+    document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'" />';
+    }
+    );
+  }
+    console.log("ml5Version- ",ml5.version);
+
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/_Ah6R24TBX/model.json',modelloaded);
+
+    function modelloaded()
+    {
+console.log("your model is loaded");
+
+    }
+
+    function check()
+    {
+      img=document.getElementById("captured_image");
+      classifier.classify(img,gotResult);
+
+    }
+
+    function gotResult(error,results)
+    {
+      if(error){
+      console.log(error);
+      }
+
+      else(results)
+      
+        
+        console.log(results)
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+         
+      }
